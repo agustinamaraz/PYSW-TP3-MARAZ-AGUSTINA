735ff1c91c4355bf63d6ede83285b59373cd72e3
agustinamaraz <102815348+agustinamaraz@users.noreply.github.com>
2023-04-18 19:32:59 -0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'PYSW-TP3-MARAZ-AGUSTINA';
+}
