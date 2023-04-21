d5c1dfac5050ec95166b2f07a3eb3af59ceceb00
agustinamaraz <102815348+agustinamaraz@users.noreply.github.com>
2023-04-21 15:38:20 -0300

    commit

--- src/app/components/punto1/punto1.component.ts
+++ src/app/components/punto1/punto1.component.ts
@@ -13,11 +13,11 @@ export class Punto1Component implements OnInit {
   constructor() { 
     this.productos = [
       {nombre: 'notebook asus 13L', descripcion: 'disco 40GB, 15pulgadas', img: 'imagen1.jpg', precio: 45.5},
-      {nombre: 'Monitor LG 14', descripcion: 'texto descriptivo producto 02', img: 'imagen2.jpg', precio: 99},
-      {nombre: 'Monitor LG 55', descripcion: 'texto descriptivo producto 02', img: 'imagen3.jpg', precio: 99},
-      {nombre: 'Monitor HP', descripcion: 'texto descriptivo producto 02', img: 'imagen1.jpg', precio: 99},
-      {nombre: 'Monitor HP', descripcion: 'texto descriptivo producto 02', img: 'imagen2.jpg', precio: 99},
-      {nombre: 'Monitor HP', descripcion: 'texto descriptivo producto 02', img: 'imagen3.jpg', precio: 99}
+      {nombre: 'Monitor LG 14', descripcion: 'texto descriptivo producto 02', img: 'imagen2.jpg', precio: 100},
+      {nombre: 'Monitor LG 55', descripcion: 'texto descriptivo producto 02', img: 'imagen3.jpg', precio: 150},
+      {nombre: 'Monitor HP', descripcion: 'texto descriptivo producto 02', img: 'imagen1.jpg', precio: 160},
+      {nombre: 'Monitor HP', descripcion: 'texto descriptivo producto 02', img: 'imagen2.jpg', precio: 170},
+      {nombre: 'Monitor HP', descripcion: 'texto descriptivo producto 02', img: 'imagen3.jpg', precio: 200}
     ];
 
   }
@@ -25,10 +25,14 @@ export class Punto1Component implements OnInit {
   ngOnInit(): void {
   }
 
-  public agregarProducto(producto: any)
+  public agregarAlCarrito(producto: any): void
   {
     this.total_plata+=producto.precio;
-    this.carrito.push({'noombre':producto.nombre,'descripcion':producto.descripcion});
+    this.carrito.push(producto);
+  }
+
+  public verCarrito() {
+    
   }
 
 }
