4670ce52e1bd87f519473eba10c187e68d82a831
agustinamaraz <102815348+agustinamaraz@users.noreply.github.com>
2023-04-24 00:59:38 -0300

    se agrego para que salga la opcion correcta

--- src/app/components/punto2/punto2.component.ts
+++ src/app/components/punto2/punto2.component.ts
@@ -6,107 +6,120 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./punto2.component.css']
 })
 export class Punto2Component implements OnInit {
-  gameStart=false;
-  gameOver!:boolean;
-  wordList:Array<any>;
-  optionList:Array<string>;
-  currentWord!:any;
-  currentOption!:string;
-  correctAnswers!:number;
-  incorrectAnswers!:number;
-  currentIteration!:number;
-  randomNumbers:Array<number>;
+  gameStart = false;
+  gameOver!: boolean;
+  wordList: Array<any>;
+  optionList: Array<string>;
+  currentWord!: any;
+  currentOption!: string;
+  correctAnswers!: number;
+  incorrectAnswers!: number;
+  currentIteration!: number;
+  randomNumbers: Array<number>;
 
   constructor() {
     this.randomNumbers = [];
     this.wordList = [
-      {name:'hoja',v:2,c:2,s:2,l:4},
-      {name:'manzana',v:3,c:4,s:3,l:7},
-      {name:'batalladora',v:5,c:6,s:5,l:11},
-      {name:'planta',v:2,c:4,s:2,l:6},
-      {name:'guitarra',v:4,c:4,s:3,l:8},
-      {name:'luz',v:1,c:2,s:1,l:3},
-      {name:'bandera',v:3,c:4,s:3,l:7},
-      {name:'diente',v:3,c:3,s:2,l:6},
-      {name:'programacion',v:5,c:7,s:4,l:12},
-      {name:'cielo',v:3,c:2,s:2,l:5}
+      { name: 'hoja', v: 2, c: 2, s: 2, l: 4 },
+      { name: 'manzana', v: 3, c: 4, s: 3, l: 7 },
+      { name: 'batalladora', v: 5, c: 6, s: 5, l: 11 },
+      { name: 'planta', v: 2, c: 4, s: 2, l: 6 },
+      { name: 'guitarra', v: 4, c: 4, s: 3, l: 8 },
+      { name: 'luz', v: 1, c: 2, s: 1, l: 3 },
+      { name: 'bandera', v: 3, c: 4, s: 3, l: 7 },
+      { name: 'diente', v: 3, c: 3, s: 2, l: 6 },
+      { name: 'programacion', v: 5, c: 7, s: 4, l: 12 },
+      { name: 'cielo', v: 3, c: 2, s: 2, l: 5 }
     ],
-    this.optionList = ['vocales','consonantes','letras','silabas']
-   }
+      this.optionList = ['vocales', 'consonantes', 'letras', 'silabas']
+  }
 
   ngOnInit(): void {
   }
 
-  public generateRandomNumber(){
-    return Math.floor(Math.random() * 12)+1;
+  public generateRandomNumber(min: number, max: number) {
+    return Math.floor(Math.random() * (max - min)) + min; //el valor maximo se excluye, o sea que solo toma la unidad anterior a ese numero (max=13 los numeros solo van a llegar hasta 12)
   }
 
-  public generateUniqueRandomNumbers(){
+  public generateUniqueRandomNumbers() {
     this.randomNumbers = [];
-    let i=0,num=0;
+    let i = 0, num = 0;
 
-   while(i<4){
-    num = this.generateRandomNumber();
-    if(!(this.randomNumbers.includes(num))){
-      this.randomNumbers.push(num);
-      i++;
+    while (i < 4) {
+      num = this.generateRandomNumber(1, 13);
+      if (!(this.randomNumbers.includes(num))) {
+        this.randomNumbers.push(num);
+        i++;
+      }
     }
-   }
 
+    num = this.generateRandomNumber(0, 3);
+    if (this.currentOption === 'vocales' && num !== this.randomNumbers[num]) {
+      this.randomNumbers[num] = this.wordList[this.currentIteration].v;
+    }
+    if (this.currentOption === 'consonantes' && !(this.randomNumbers.includes(num))) {
+      this.randomNumbers[num] = this.wordList[this.currentIteration].c;
+    }
+    if (this.currentOption === 'letras' && !(this.randomNumbers.includes(num))) {
+      this.randomNumbers[num] = this.wordList[this.currentIteration].l;
+    }
+    if (this.currentOption === 'silabas' && !(this.randomNumbers.includes(num))) {
+      this.randomNumbers[num] = this.wordList[this.currentIteration].s;
+    }
   }
 
-  public initializeGame(){
-    this.currentIteration=0;
-    this.gameStart=true;
-    this.gameOver=false;
-    this.correctAnswers=0;
-    this.incorrectAnswers=0; 
+  public initializeGame() {
+    this.currentIteration = 0;
+    this.gameStart = true;
+    this.gameOver = false;
+    this.correctAnswers = 0;
+    this.incorrectAnswers = 0;
 
     this.currentWord = this.wordList[this.currentIteration].name;
-    this.currentOption = this.optionList[Math.floor(Math.random()*4)];
+    this.currentOption = this.optionList[Math.floor(Math.random() * 4)];
     this.generateUniqueRandomNumbers();
   }
 
-  public startGame(answer:number):void{
-    if(this.currentIteration < 10){
-      if(this.currentOption === this.optionList[0]){
-        if(answer === this.wordList[this.currentIteration].v){
+  public startGame(answer: number): void {
+    if (this.currentIteration < 10) {
+      if (this.currentOption === this.optionList[0]) {
+        if (answer === this.wordList[this.currentIteration].v) {
           this.correctAnswers++;
-        }else{
+        } else {
           this.incorrectAnswers++;
         }
       }
-      if(this.currentOption === this.optionList[1]){
-        if(answer === this.wordList[this.currentIteration].c){
+      if (this.currentOption === this.optionList[1]) {
+        if (answer === this.wordList[this.currentIteration].c) {
           this.correctAnswers++;
-        }else{
+        } else {
           this.incorrectAnswers++;
         }
       }
-      if(this.currentOption === this.optionList[2]){
-        if(answer === this.wordList[this.currentIteration].l){
+      if (this.currentOption === this.optionList[2]) {
+        if (answer === this.wordList[this.currentIteration].l) {
           this.correctAnswers++;
-        }else{
+        } else {
           this.incorrectAnswers++;
         }
       }
-      if(this.currentOption === this.wordList[this.currentIteration].s){
-        if(answer === this.currentWord.s){
+      if (this.currentOption === this.wordList[this.currentIteration].s) {
+        if (answer === this.currentWord.s) {
           this.correctAnswers++;
-        }else{
+        } else {
           this.incorrectAnswers++;
         }
       }
       this.currentIteration++;
 
-      if(this.currentIteration<10){
+      if (this.currentIteration < 10) {
         this.currentWord = this.wordList[this.currentIteration].name;
         this.generateUniqueRandomNumbers();
-      }else{
-        this.gameOver=true;
+      } else {
+        this.gameOver = true;
       }
-        
-    }else{
+
+    } else {
       this.gameOver = true;
     }
   }
