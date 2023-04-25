c58530df2352114d651ce1b415dd22e2f9bede75
agustinamaraz <102815348+agustinamaraz@users.noreply.github.com>
2023-04-25 10:51:19 -0300

    pagec-zodiacSigns-finished

--- src/app/components/punto3/pagea/pagea.component.ts
+++ src/app/components/punto3/pagea/pagea.component.ts
@@ -6,8 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pagea.component.css']
 })
 export class PageaComponent implements OnInit {
+  cards:Array<any>;
 
-  constructor() { }
+  constructor() { 
+    this.cards = [
+      {title: 'DONDE QUIERO ESTAR', artist: 'QUEVEDO', img: 'team5.jpg'},
+      {title: 'TQG', artist: 'KAROL G, Shakira', img: 'team5.jpg'},
+      {title: 'Bellodrama', artist: 'Ana Mena', img: 'team5.jpg'},
+      {title: 'TAMO EN NOTA', artist: 'Rauw Alejandro ft. Angel Dior', img: 'team5.jpg'},
+      {title: 'Tormenta', artist: 'Gorillaz', img: 'team5.jpg'},
+      {title: 'EPIC SOLETE', artist: 'THE TYETS', img: 'team5.jpg'}
+    ]
+  }
 
   ngOnInit(): void {
   }
--- src/app/components/punto3/pagec/pagec.component.ts
+++ src/app/components/punto3/pagec/pagec.component.ts
@@ -6,8 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pagec.component.css']
 })
 export class PagecComponent implements OnInit {
-
-  constructor() { }
+  signs:Array<any>;
+  constructor() { 
+    this.signs = [
+      {name:'leo',img:'team5.jpg'},
+      {name:'aries',img:'team5.jpg'},
+      {name:'sagitario',img:'team5.jpg'},
+      {name:'acuario',img:'team5.jpg'},
+      {name:'tauro',img:'team5.jpg'},
+      {name:'geminis',img:'team5.jpg'},
+      {name:'libra',img:'team5.jpg'},
+      {name:'escorpio',img:'team5.jpg'},
+      {name:'cancer',img:'team5.jpg'}
+    ]
+  }
 
   ngOnInit(): void {
   }
